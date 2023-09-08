9b8e9fc68374c9a49cfdecafdf6ebfb6e110959f
yabeen0126 <141331767+yabeen0126@users.noreply.github.com>
2023-09-08 21:19:05 +0900

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 
 	// localhost = DB주소, web=DB계정아이디, 1234=DB계정비밀번호, post_board="DB이름"
 	//$db = new mysqli("localhost","yabeen","1q2w3e4r","Kknockboard"); 
-	$db = new mysqli("localhost","habin","qwert1234","database"); 
+	$db = new mysqli("localhost","habin","qwert1234","Kknockboard"); 
 	$db->set_charset("utf8");
 
 	function mq($sql)
