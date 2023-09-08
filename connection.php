dccbbb2d8f78e9788e67bde1e4cc1d8721036660
yabeen0126 <141331767+yabeen0126@users.noreply.github.com>
2023-09-08 21:18:24 +0900

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "habin";
     $password = "qwert1234";
-    $db_name = "database";
+    $db_name = "Kknockboard";
     $conn = new mysqli($servername, $username, $password, $db_name, 3306);
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
